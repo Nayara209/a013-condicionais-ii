aece0c8328a59b73dbf4182a2924e97a1c99c9d4
Nayara <nayaraazevwdo@gmail.com>
2022-08-10 14:57:36 -0300

    exercicio-a013

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,2 +1,18 @@
-const num = //seu numero aqui
+const num = Number(prompt("Digite um número aqui"))
+
+if(num %2===0){
+    if(num %3===0){
+       console.log("É divisível por 2 e 3")
+    }else{
+        console.log("Não é divisível")
+    }
+}else{
+    console.log("Não é divisível")
+    }
+
+if(num %2===0 && num %3===0){
+    console.log("sim")
+}
+    
+
 
